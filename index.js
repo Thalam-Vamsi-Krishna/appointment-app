46b26573578470d9a4107d39fd0544529795deb3
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2022-12-30 13:31:30 +0530

    successfully get the user details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById("userForm").addEventListener("submit", addUser);
 // initial array of users, reading from localStorage
-const users = JSON.parse(localStorage.getItem("users")) || [];
+//const users = JSON.parse(localStorage.getItem("users")) || [];
 function addUser(e) {
   e.preventDefault();
   // get name, phone_no
@@ -10,9 +10,8 @@ function addUser(e) {
     const user = {
       name,
       phone_no,
-      id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+      //id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
     };
-    users.push(user);
     axios
       .post(
         "https://crudcrud.com/api/acde7f51dbf248f4876995e1a5c1f0ee/appointmentData",
@@ -21,6 +20,7 @@ function addUser(e) {
       .then((response) => {
         console.log(response);
       });
+    //users.push(user);
     //local storage
     //localStorage.setItem("users", JSON.stringify(users));
   } else {
@@ -33,7 +33,23 @@ function addUser(e) {
 const showUsers = () => {
   const userTable = document.getElementById("userTable");
   userTable.innerHTML = "";
-  for (let i = 0; i < users.length; i++) {
+  axios
+    .get(
+      "https://crudcrud.com/api/acde7f51dbf248f4876995e1a5c1f0ee/appointmentData"
+    )
+    .then((response) => {
+      for (let i = 0; i < response.data.length; i++) {
+        userTable.innerHTML += `
+            <tr>
+                <td>${response.data[i].name}</td>
+                <td>${response.data[i].phone_no}</td>
+                <td><a class="editButton"onclick="editUser(${response.data[i].id})">Edit<br><a class="deleteButton" onclick="deleteUser(${response.data[i].id})">
+                    Delete</td>
+            </tr>
+        `;
+      }
+    });
+  /*for (let i = 0; i < users.length; i++) {
     userTable.innerHTML += `
             <tr>
                 <td>${users[i].name}</td>
@@ -42,24 +58,24 @@ const showUsers = () => {
                     Delete</td>
             </tr>
         `;
-  }
+  }*/
 };
 const editUser = (id) => {
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].id == id) {
-      document.getElementById("name").value = users[i].name;
-      document.getElementById("phone").value = users[i].phone_no;
+  for (let i = 0; i < response.data.length; i++) {
+    if (response.data[i].id == id) {
+      document.getElementById("name").value = response.data[i].name;
+      document.getElementById("phone").value = response.data[i].phone_no;
     }
   }
 };
 const deleteUser = (id) => {
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].id == id) {
+  for (let i = 0; i < response.data.length; i++) {
+    if (response.data[i].id == id) {
       users.splice(i, 1);
     }
   }
   // localStorage
-  localStorage.setItem("expenses", JSON.stringify(users));
+  //localStorage.setItem("expenses", JSON.stringify(users));
   showUsers();
 };
 showUsers();
